025143c7969738bb511b08b7e71f021d52f5ba0b
Vivanto <vivanto@qq.com>
2026-06-23 16:42:35 +0800

    Now can correctly create song class

--- songfactory.cpp
+++ songfactory.cpp
@@ -5,6 +5,8 @@
 #include <QUrl>
 #include <QList>
 #include <QFileInfo>
+#include <QEventLoop>
+#include <QTimer>
 
 SongFactory::SongFactory(QObject *parent)
     : QObject{parent}
@@ -15,17 +17,39 @@ Song* SongFactory::buildSong(const QString& url)
     QMediaPlayer player;
     player.setSource(QUrl(url));
     
+    QEventLoop loop;
+    QTimer timer;
+    timer.setSingleShot(true);
+    timer.setInterval(3000);
+    
+    QObject::connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
+    QObject::connect(&player, &QMediaPlayer::mediaStatusChanged, [&](QMediaPlayer::MediaStatus status) {
+        if (status == QMediaPlayer::LoadedMedia || 
+            status == QMediaPlayer::InvalidMedia || 
+            status == QMediaPlayer::EndOfMedia) {
+            loop.quit();
+        }
+    });
+    
+    timer.start();
+    loop.exec();
+    
     QMediaMetaData metaData = player.metaData();
     QString name = metaData.value(QMediaMetaData::Title).toString();
-    QString singer = metaData.value(QMediaMetaData::Author).toString();
+    QStringList singerList = metaData.value(QMediaMetaData::Author).toStringList();
     QString album = metaData.value(QMediaMetaData::AlbumTitle).toString();
     int duration = metaData.value(QMediaMetaData::Duration).toInt();
-    
+    QString singer;
+
     if (name.isEmpty()) {
         name = "Unknown";
     }
-    if (singer.isEmpty()) {
+    if (singerList.isEmpty()) {
         singer = "Unknown";
+    }
+    else
+    {
+        singer = singerList.at(0);
     }
     if (album.isEmpty()) {
         album = "Unknown Album";
